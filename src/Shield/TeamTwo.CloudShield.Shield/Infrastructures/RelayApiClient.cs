cc70175603af9e2d02b9fb04bcda5a31a14507d3
HitzSPB <daniel.oesterbro@gmail.com>
2020-08-04 22:06:13 +0200

    Added Proxy Relay Call

---
+++ src/Shield/TeamTwo.CloudShield.Shield/Infrastructures/RelayApiClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Relay;
+using TeamTwo.CloudShield.Shield.Service.Models;
+
+namespace TeamTwo.CloudShield.Shield.Infrastructures
+{
+  public class RelayApiClient : IRelayApiClient
+  {
+    private readonly HttpClient _httpClient;
+    public RelayApiClient(HttpClient httpClient)
+    {
+      _httpClient = httpClient;
+    }
+    async Task<HttpResponseMessage> IRelayApiClient.RelayCallAsync(RelayCallDto relayCallDto)
+    {
+      await SetupHttpClientAsync(relayCallDto);
+      var request = new HttpRequestMessage(relayCallDto.HttpMethod, new Uri(relayCallDto.HybridConnection.HybridConnectionUrl, relayCallDto.Url));
+      request.Content = new StringContent(relayCallDto.BodyContent);
+
+      HttpResponseMessage response = await _httpClient.SendAsync(request);
+
+      return response;
+    }
+
+    private async Task SetupHttpClientAsync(RelayCallDto relayCallDto)
+    {
+      PolicyDto policyDto = relayCallDto.HybridConnection.PolicyDtos.First(x => x.PolicyType == PolicyClaim.Send);
+      var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(policyDto.PolicyName, policyDto.PolicyKey);
+      var token = (await tokenProvider.GetTokenAsync(relayCallDto.HybridConnection.HybridConnectionUrl.AbsoluteUri, TimeSpan.FromHours(1))).TokenString;
+      _httpClient.DefaultRequestHeaders.Add("ServiceBusAuthorization", token);
+    }
+  }
+}
--- src/Shield/TeamTwo.CloudShield.Shield/Services/Models/RelayCallDto.cs
+++ src/Shield/TeamTwo.CloudShield.Shield/Services/Models/RelayCallDto.cs
@@ -5,17 +5,19 @@ namespace TeamTwo.CloudShield.Shield.Service.Models
 {
   public class RelayCallDto
   {
-    public RelayCallDto(HybridConnectionDto hybridConnection, string bodyContent, HttpMethod httpMethod, IHeaderDictionary httpHeaders)
+    public RelayCallDto(HybridConnectionDto hybridConnection, string bodyContent, HttpMethod httpMethod, IHeaderDictionary httpHeaders, string url)
     {
       HybridConnection = hybridConnection;
       BodyContent = bodyContent;
       HttpMethod = httpMethod;
       HttpHeaders = httpHeaders;
+      Url = url;
     }
 
     public HybridConnectionDto HybridConnection { get; set; }
     public string BodyContent { get; set; }
     public HttpMethod HttpMethod { get; set; }
     public IHeaderDictionary HttpHeaders { get; set; }
+    public string Url { get; set; }
   }
 }
