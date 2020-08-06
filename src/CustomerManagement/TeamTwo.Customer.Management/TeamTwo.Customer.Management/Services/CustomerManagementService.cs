38a33e329e6406071f2213d3251271174c2965d5
HitzSPB <daniel.oesterbro@gmail.com>
2020-08-06 23:32:29 +0200

    Added more structure to customer project.

--- src/CustomerManagement/TeamTwo.Customer.Management/TeamTwo.Customer.Management/Services/CustomerManagementService.cs
+++ src/CustomerManagement/TeamTwo.Customer.Management/TeamTwo.Customer.Management/Services/CustomerManagementService.cs
@@ -1,7 +1,23 @@
-﻿namespace TeamTwo.Customer.Management.Services
+﻿using System.Threading.Tasks;
+using TeamTwo.Customer.Management.Infrastructure;
+
+namespace TeamTwo.Customer.Management.Services
 {
   public class CustomerManagementService : ICustomerManagementService
   {
+    private readonly ICustomerManagementStorageApiClient _customerManagementStorageApiClient;
+    public CustomerManagementService(ICustomerManagementStorageApiClient customerManagementStorageApiClient)
+    {
+      _customerManagementStorageApiClient = customerManagementStorageApiClient;
+    }
+    Task<string> ICustomerManagementService.GetCustomerInformation()
+    {
+      throw new System.NotImplementedException();
+    }
 
+    Task<string> ICustomerManagementService.StoreCustomerInformation()
+    {
+      throw new System.NotImplementedException();
+    }
   }
 }
