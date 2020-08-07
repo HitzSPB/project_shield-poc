c19ea026558d29c510cac15bc024ead5fae4cd6f
HitzSPB <daniel.oesterbro@gmail.com>
2020-08-07 22:27:16 +0200

    Added methods to customermanagementstorage

--- src/CustomerManagement/TeamTwo.Customer.Management/TeamTwo.Customer.Management/Infrastructure/ICustomerManagementStorageApiClient.cs
+++ src/CustomerManagement/TeamTwo.Customer.Management/TeamTwo.Customer.Management/Infrastructure/ICustomerManagementStorageApiClient.cs
@@ -1,6 +1,11 @@
-﻿namespace TeamTwo.Customer.Management.Infrastructure
+﻿using System.Threading.Tasks;
+using Microsoft.Build.Utilities;
+
+namespace TeamTwo.Customer.Management.Infrastructure
 {
   public interface ICustomerManagementStorageApiClient
   {
+    Task<object> StoreCustomer(string custonemerId);
+    Task<object> GetCustomer(string tenantId);
   }
 }
