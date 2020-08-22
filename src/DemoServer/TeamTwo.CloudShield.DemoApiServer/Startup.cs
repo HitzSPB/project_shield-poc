384ddb9e3bb5dda6516d620bff65517fc1e426cc
HitzSPB <daniel.oesterbro@gmail.com>
2020-08-22 02:53:03 +0200

    Removed middelware that are not supposed to be in the setup

--- src/DemoServer/TeamTwo.CloudShield.DemoApiServer/Startup.cs
+++ src/DemoServer/TeamTwo.CloudShield.DemoApiServer/Startup.cs
@@ -24,8 +24,6 @@ namespace TeamTwo.CloudShield.DemoApiServer
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-      if (env.IsDevelopment())
-        app.UseDeveloperExceptionPage();
 
       app.UseHttpsRedirection();
 
